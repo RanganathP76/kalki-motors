835c9b6fa357ce0881e9f4f4b452109cf3a005a1
Ranganath P <ranagathgowdap07@gmail.com>
2025-08-25 19:36:31 +0530

    Update React app to PWA with service worker, manifest, and INR currency

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css"; // optional reset
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 createRoot(document.getElementById("root")).render(<App />);
+
+serviceWorkerRegistration.register();
+
