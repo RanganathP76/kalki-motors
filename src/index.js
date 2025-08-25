24bec813045cf9cd5ec52e4f7b130ed3b23f458a
Ranganath P <ranagathgowdap07@gmail.com>
2025-08-25 17:55:55 +0530

    Latest updates and fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css"; 
-import { HelmetProvider } from "react-helmet-async";
+import "./index.css"; // optional reset
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
